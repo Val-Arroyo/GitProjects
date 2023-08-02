dea78d22f4ba236888e48e9221dbc281d0a5ac4a
valarroyo-gif <valarroyo39@gmail.com>
2023-08-02 19:19:38 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -1,6 +1,1 @@
-print("Software Architect\n");
-print("Business Analyst\n");
-print("Data Analyst\n");
-print("Software Developer\n");
-print("Software Engineer\n");
-print("Business Intelligence Analyst\n");
+Straw_Hat_Captain
