d73ce438de4154d77cc65555f9686ce9ffb3f772
valarroyo-gif <valarroyo39@gmail.com>
2023-08-02 19:22:01 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -3,6 +3,6 @@ BlackBeard_Captain = "Marshall D. Teech";
 RedHaired_Captain = "Shanks";
 WhiteBeard_Captain = "Edward Newgate";
 BigMOM_Captain = "Charlotte Linlin";
-
+Beast_Captain = "Kaido"
 
 print(Straw_Hat_Captain);
