de1e4335558b186e3c079c0c353693ad7eaaede4
valarroyo-gif <valarroyo39@gmail.com>
2023-08-02 19:21:18 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -1,6 +1,6 @@
 Straw_Hat_Captain = "Monkey D. Luffy";  
 BlackBeard_Captain = "Marshall D. Teech";
 RedHaired_Captain = "Shanks";
-WhiteBeard_
+WhiteBeard_Captain = "";
 
 print(Straw_Hat_Captain);
