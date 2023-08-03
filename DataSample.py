9e7626989017b6baf43c11dbb2354b502c388cdc
valarroyo-gif <valarroyo39@gmail.com>
2023-08-03 15:25:34 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -1,3 +1,4 @@
+#Variables display
 Straw_Hat_Captain = "Monkey D. Luffy";  
 BlackBeard_Captain = "Marshall D. Teech";
 RedHaired_Captain = "Shanks";
@@ -5,9 +6,11 @@ WhiteBeard_Captain = "Edward Newgate";
 BigMOM_Captain = "Charlotte Linlin";
 Beast_Captain = "Kaido";
 
+#Printing those variables
 print(Straw_Hat_Captain);
 print(BlackBeard_Captain);
 print(RedHaired_Captain);
 print(WhiteBeard_Captain);
 print(BigMOM_Captain);
 print(Beast_Captain);
+
