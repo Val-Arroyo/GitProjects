808068775a8d591361625469a29b6ad052b260b5
valarroyo-gif <valarroyo39@gmail.com>
2023-08-05 20:05:28 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -1,35 +1,37 @@
-#Variables display
-Straw_Hat_Captain = "Monkey D. Luffy";  
-BlackBeard_Captain = "Marshall D. Teech";
-RedHaired_Captain = "Shanks";
-WhiteBeard_Captain = "Edward Newgate";
-BigMOM_Captain = "Charlotte Linlin";
-Beast_Captain = "Kaido";
+def onepiece():
+
+
+    #Variables display
+    Straw_Hat_Captain = "Monkey D. Luffy"
+    BlackBeard_Captain = "Marshall D. Teech"
+    RedHaired_Captain = "Shanks"
+    WhiteBeard_Captain = "Edward Newgate"
+    BigMOM_Captain = "Charlotte Linlin"
+    Beast_Captain = "Kaido"
 
 #Printing those variables
-print(Straw_Hat_Captain);
-print(BlackBeard_Captain);
-print(RedHaired_Captain);
-print(WhiteBeard_Captain);
-print(BigMOM_Captain);
-print(Beast_Captain);
+    print(Straw_Hat_Captain)
+    print(BlackBeard_Captain)
+    print(RedHaired_Captain)
+    print(WhiteBeard_Captain)
+    print(BigMOM_Captain)
+    print(Beast_Captain)
 
 #Crew Variables
-Straw_Hat_Crew = ["Zoro", "Sanji", "Nami", "Robin", "Franky", "Chopper", "Usopp", "Jimbei", "Yamato"];
-BlackBeard_Crew = [];
-RedHaired_Crew = [];
-WhiteBeard_Crew = ["Marco", "Ace", "Jozu"];
-BigMOM_Crew = ["Peros Pero"];
-Beast_Crew = ["King", "Queen"];
+    Straw_Hat_Crew = ["Zoro", "Sanji", "Nami", "Robin",
+                  "Franky", "Chopper", "Usopp", "Jimbei", "Yamato"]
+    BlackBeard_Crew = []
+    RedHaired_Crew = []
+    WhiteBeard_Crew = ["Marco", "Ace", "Jozu"]
+    BigMOM_Crew = ["Peros Pero"]
+    Beast_Crew = ["King", "Queen"]
 
 #Print Crew Variables
-print(Straw_Hat_Crew);
-print(BlackBeard_Crew);
-print(RedHaired_Crew);
-print(WhiteBeard_Crew);
-print(BigMOM_Crew);
-print(Beast_Crew);
-
-def onepiece():
+print(Straw_Hat_Crew)
+print(BlackBeard_Crew)
+print(RedHaired_Crew)
+print(WhiteBeard_Crew)
+print(BigMOM_Crew)
+print(Beast_Crew)
 
 name = input();
