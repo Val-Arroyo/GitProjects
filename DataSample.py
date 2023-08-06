39bb1cf97e90cf7a9f9e083c8d6b9f1e2bc2307b
valarroyo-gif <valarroyo39@gmail.com>
2023-08-06 16:46:03 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -42,7 +42,7 @@ print("Your full name  is: ", firstname, middlename , lastname);
 print("Your age is" , age);
 
 if(age >= 18):
-    print();
+    print("");
     print("Enter the night club");
     print("");
 else:
