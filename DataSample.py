816b33cc8eebb2df40f0d880da68c1a34a463960
valarroyo-gif <valarroyo39@gmail.com>
2023-08-08 16:17:05 +1200

    Update DataSample.py

--- DataSample.py
+++ DataSample.py
@@ -1,53 +1,5 @@
 import math
-def onepiece():
+
+print (math.pow(64,2));
 
 
-    #Variables display
-    Straw_Hat_Captain = "Monkey D. Luffy"
-    BlackBeard_Captain = "Marshall D. Teech"
-    RedHaired_Captain = "Shanks"
-    WhiteBeard_Captain = "Edward Newgate"
-    BigMOM_Captain = "Charlotte Linlin"
-    Beast_Captain = "Kaido"
-
-#Printing those variables
-    print(Straw_Hat_Captain)
-    print(BlackBeard_Captain)
-    print(RedHaired_Captain)
-    print(WhiteBeard_Captain)
-    print(BigMOM_Captain)
-    print(Beast_Captain)
-
-#Crew Variables
-    Straw_Hat_Crew = ["Zoro", "Sanji", "Nami", "Robin","Franky", "Chopper", "Usopp", "Jimbei", "Yamato"]
-    BlackBeard_Crew = []
-    RedHaired_Crew = []
-    WhiteBeard_Crew = ["Marco", "Ace", "Jozu"]
-    BigMOM_Crew = ["Peros Pero"]
-    Beast_Crew = ["King", "Queen"]
-
-#Print Crew Variables
-    print(Straw_Hat_Crew)
-    print(BlackBeard_Crew)
-    print(RedHaired_Crew)
-    print(WhiteBeard_Crew)
-    print(BigMOM_Crew)
-    print(Beast_Crew)
-
-firstname = input("Enter your first name: ");
-middlename = input("Enter your middle name: ");
-lastname = input("Enter your last time: ");
-age = int(input("Enter your age: "));
-
-print("\n");
-print("Your full name  is: ", firstname, middlename , lastname);
-print("Your age is" , age);
-
-if(age >= 18):
-    print("Since you are above 18");
-    print("Enter the night club");
-    print("Enjoy your time");
-else:
-    print("You are not above 18 yet")
-    print("You may not enter");
-    print("Comeback when you are 18 or over");
