f1344b3ba3c7433eb448254ecf3bf107b3ae2920
valarroyo-gif <valarroyo39@gmail.com>
2023-08-09 18:50:28 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16]);
-
-plt.ylabel(['y numbers']);
-plt.xlabel(['x numbers']);
-
-plt.show();
+x = [1,2,3]
+# corresponding y axis values
+y = [2,4,1]
+  
+# plotting the points 
+plt.plot(x, y)
+  
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+  
+# giving a title to my graph
+plt.title('My first graph!')
+  
+# function to show the plot
+plt.show()
