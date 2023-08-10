9343368a31f477fbf421eab2078ec41315ed23f9
valarroyo-gif <valarroyo39@gmail.com>
2023-08-10 16:10:59 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -5,24 +5,25 @@ import numpy as np
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 y = np.array([1, 3, 5, 7, 9, 2, 4, 6, 8, 10]);
 
-plt.subplot(1, 2, 1)
-plt.plot(x,y ,color = 'red', marker = 'o');
+plt.subplot(1, 3, 1)
+plt.plot(x,y ,color = 'red', marker = '*');
 plt.title("SALES");
 
 #plot 2:
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 y = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110])
 
-plt.subplot(1, 2, 2);   
-plt.plot(x,y, color = 'blue', marker = "o");
+plt.subplot(1, 3, 2);   
+plt.plot(x,y, color = 'blue', marker = "*");
 plt.title("INCOME");
 
 #plot 3:
 x = np.array([0, 10, 20, 30, 40]);
-y = np.array([10000, 20000, 30000, 40000, 50000]);
+y = np.array([10000, 30000, 50000, 20000, 60000]);
 
-plt.subplot(1, 2, 3);
-plt.plot(x, y, color = 'black', marker = 'o');
+plt.subplot(1, 3, 3);
+plt.plot(x, y, color = 'black', marker = '*');
 plt.title("Investments");
 
+plt.suptitle("MONEY GRAPHS");
 plt.show();
