0105328c1c94c3b231337d7a58cd4dbed155d51b
valarroyo-gif <valarroyo39@gmail.com>
2023-08-10 15:42:42 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -10,7 +10,7 @@ plt.plot(x,y ,color = 'red', marker = 'o');
 plt.title("SALES")
 
 #plot 2:
-x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9,])
 y = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 
 plt.subplot(1, 2, 2)
