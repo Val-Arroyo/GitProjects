0de2c7ee4ac92569bbaf69bbe0bb90098a7980c7
valarroyo-gif <valarroyo39@gmail.com>
2023-08-10 15:41:12 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #plot 1:
-x = np.array([0, 1, 2, 3, 4, 5])
-y = np.array([3, 8, 1, 10, 21, 1]);
+x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y = np.array([3, 8, 1, 10, 4, 1, 7, 6, 4, 5, ]);
 
 plt.subplot(1, 2, 1)
 plt.plot(x,y ,color = 'red', marker = 'o');
