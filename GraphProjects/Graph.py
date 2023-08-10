79868cf1807fcb2b41972ab123171584c448a995
valarroyo-gif <valarroyo39@gmail.com>
2023-08-10 15:43:03 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -13,7 +13,7 @@ plt.title("SALES");
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 y = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 2, 2);
 plt.plot(x,y, color = 'blue', marker = "o");
 plt.title("INCOME");
 
