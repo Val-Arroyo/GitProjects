df676ebd7a4ff1851a628405651a36fe0dfbde73
valarroyo-gif <valarroyo39@gmail.com>
2023-08-13 13:38:07 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -28,6 +28,7 @@ y = np.array([10000, 30000, 50000, 20000, 60000]);
 plt.subplot(1, 3, 3);
 plt.plot(x, y, color = 'black', marker = '*');
 plt.xlabel();
+plt.ylabel();
 plt.title("Investments");
 
 plt.suptitle("MONEY GRAPHS");
