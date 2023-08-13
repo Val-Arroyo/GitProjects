8123236c83dbf3dfb2c579bb8db6d5b9e1266979
valarroyo-gif <valarroyo39@gmail.com>
2023-08-13 13:51:02 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+ax = plt.axes();
 #plot 1:
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 y = np.array([1, 3, 5, 7, 9, 2, 4, 6, 8, 10]);
 
-plt.subplot(1, 3, 1)
+plt.subplot(1, 3, 1);
+plt.figure(facecolor="red");
 plt.plot(x,y ,color = 'red', marker = '*');
 plt.xlabel("Money Income");
 plt.ylabel("Yearly Income");
@@ -16,6 +19,7 @@ x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 y = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110])
 
 plt.subplot(1, 3, 2);   
+plt.figure(facecolor=);
 plt.plot(x,y, color = 'blue', marker = "*");
 plt.xlabel("Money Income");
 plt.ylabel("Yearly Income");
@@ -25,7 +29,7 @@ plt.title("INCOME");
 x = np.array([0, 10, 20, 30, 40]);
 y = np.array([10000, 30000, 50000, 20000, 60000]);
 
-plt.subplot(1, 4, 3);
+plt.subplot(1, 3, 3);
 plt.plot(x, y, color = 'black', marker = '*');
 plt.xlabel("Investments return");
 plt.ylabel("Yearly income");
