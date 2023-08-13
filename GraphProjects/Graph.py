dbbb8d0e464f800a303cb655e3f5fa1fef74c587
valarroyo-gif <valarroyo39@gmail.com>
2023-08-13 13:36:22 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -7,7 +7,7 @@ y = np.array([1, 3, 5, 7, 9, 2, 4, 6, 8, 10]);
 
 plt.subplot(1, 3, 1)
 plt.plot(x,y ,color = 'red', marker = '*');
-plt.xlabel("Year");
+plt.xlabel("Money");
 plt.ylabel("Money Return");
 plt.title("SALES");
 
