1278c0fe2d601516cafffac3a447a9a43c7b9fc4
valarroyo-gif <valarroyo39@gmail.com>
2023-08-14 15:52:15 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -3,7 +3,7 @@ from  mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
 
-np.random.seed(42)
+np.random.seed(42);
 
 xs = np.random.random(100)*10+20
 
@@ -11,7 +11,7 @@ ys = np.random.random(100)*5+7
 
 zs = np.random.random(100)*15+50
 
-fig = plt.figure()
+fig = plt.figure();
 
 ax = fig.add_subplot(111, projection='3d')
 
