6d45c81e4ae9df0f5ce439e19b82f5ab7b82eb64
valarroyo-gif <valarroyo39@gmail.com>
2023-08-14 15:49:37 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -1,25 +1,12 @@
 import matplotlib.pyplot as plt
+from  mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
 
-# Using Numpy to create an array X
-X = np.arange(0, math.pi*2, 0.05);
-  
-# Assign variables to the y axis part of the curve
-y = np.sin(X);
-z = np.cos(X);
-  
-# Plotting both the curves simultaneously
-plt.plot(X, y, color='r', label='sin');
-plt.plot(X, z, color='g', label='cos');
-  
-# Naming the x-axis, y-axis and the whole graph
-plt.xlabel("Angle");
-plt.ylabel("Magnitude");
-plt.title("Sine and Cosine functions");
-  
-# Adding legend, which helps us recognize the curve according to it's color
-plt.legend();
-  
-# To load the display window
-plt.show();
+fig = plt.figure(figsize=(4,4))
+
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(2,3,4) # plot the point (2,3,4) on the figure
+
+plt.show()
