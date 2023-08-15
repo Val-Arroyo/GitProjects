48afa74c2f5a0ce33ef80be560b3c67e8bfdc0f6
valarroyo-gif <valarroyo39@gmail.com>
2023-08-15 16:20:06 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -9,4 +9,4 @@ fig, ax = plt.subplot();
 fruits = ['Apple', 'Blueberry', 'Cherry', 'Orange'];
 counts = ['40', '100', '30', '50'];
 bar_labels = ['red', 'blue', 'red', 'orange'];
-bar_colors = ['tab: red', 'tab: blue', 'tab: red', 'tab: purple']
+bar_colors = ['tab: red', 'tab: blue', 'tab: red', 'tab: orange'];
