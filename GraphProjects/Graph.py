c0f356ec0edcff0f5ee87d46fd671d1c0236c294
valarroyo-gif <valarroyo39@gmail.com>
2023-08-16 16:18:32 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -3,12 +3,13 @@ from  mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
 
-x = [10, 20, 30, 40];
-y = [10, 20, 30, 40];
+year = [1972, 1982, 1992, 2002, 2012];
+australia = [100.6, 158.61, 305.54, 394.96, 724.79];
+new_zealand = [10.5, 25.21, 58.65, 119.27, 274.87];
 
-plt.plot(x, y);
+plt.plot(year, australia, color='red', label='Australia');
+plt.plot(year, new_zealand, color='blue', label='New Zealand');
 
-plt.title("Simple Graph");
-plt.xlabel("X-Axis");
-plt.ylabel("Y-Axis");
-plt.show();
+
+plt.xlabel("Years");
+plt.ylabel("Power of consumption in kWh");
