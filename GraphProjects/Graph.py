8c2334d394fb5cab5b00021befa04e21ccf183e6
valarroyo-gif <valarroyo39@gmail.com>
2023-08-16 15:54:14 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -2,10 +2,3 @@ import matplotlib.pyplot as plt
 from  mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
-from datetime import datetime
-
-fig = plt.figure();
-
-ax = fig.add_axes([0, 0, 1, 1]);
-
-plt.show();
