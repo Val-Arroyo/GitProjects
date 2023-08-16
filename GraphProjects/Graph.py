cec011826cf5aed0289d86a8a5d570bd46ee7d93
valarroyo-gif <valarroyo39@gmail.com>
2023-08-16 16:22:04 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -7,7 +7,7 @@ year = [1972, 1982, 1992, 2002, 2012];
 australia = [100.6, 158.61, 305.54, 394.96, 724.79];
 new_zealand = [10.5, 25.21, 58.65, 119.27, 274.87];
 
-plt.plot(year, australia, color='red', label='Australia', marker="D" );
+plt.plot(year, australia, color='red', label='Australia', marker="" );
 plt.plot(year, new_zealand, color='blue', label='New Zealand', marker="D");
 
 
