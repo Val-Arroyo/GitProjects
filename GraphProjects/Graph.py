8edb60f5791aa42c69609a240585202d592507d6
valarroyo-gif <valarroyo39@gmail.com>
2023-08-17 15:23:19 +1200

    Update Graph.py

--- GraphProjects/Graph.py
+++ GraphProjects/Graph.py
@@ -4,7 +4,7 @@ import numpy as py
 #Variables between anime viewership per year
 
 year = [2000, 2005, 2010, 2015, 2020, 2025];
-One_piece = [];
+One_piece = [10, ];
 Naruto = [];
 Bleach = [];
 DragonBall = [];
