64554c02122c61a5352f6ef98edc6ec34a2335c4
valarroyo-gif <valarroyo39@gmail.com>
2023-08-18 21:26:42 +1200

    Update Subplot.py

--- GraphProjects/Studysession/Subplot.py
+++ GraphProjects/Studysession/Subplot.py
@@ -20,7 +20,7 @@ ax[0, 1].title.set_text("Double")
 ax[1, 0].title.set_text("Square")
 ax[1, 1].title.set_text("Cube")
 ax[2, 0].title.set_text("Random");
-ax[2, 1].
+ax[2, 1].title
  
 # set spacing
 fig.tight_layout()
