b8334338dc60f206ddbcaf57bd22c2764f7aea39
valarroyo-gif <valarroyo39@gmail.com>
2023-08-20 19:23:47 +1200

    Update Subplot.py

--- GraphProjects/Studysession/Subplot.py
+++ GraphProjects/Studysession/Subplot.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-y = np.array[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-plt.title("");
-plt.xlabel("");
-plt.ylabel("");
+plt.title("Sports Watch Data");
+plt.xlabel("Average Pulse");
+plt.ylabel("Calorie Burnage");
+
+plt.plot(x, y);
+plt.grid(axis = 'y');
+
+
+plt.show();
