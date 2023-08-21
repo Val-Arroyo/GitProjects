2b7c19fd3ff5beebd8b37f5d2b26361f8f66b375
valarroyo-gif <valarroyo39@gmail.com>
2023-08-21 15:40:12 +1200

    Update Subplot.py

--- GraphProjects/Studysession/Subplot.py
+++ GraphProjects/Studysession/Subplot.py
@@ -5,7 +5,7 @@ x = np.array([1, 2, 3, 4, 5]);
 y = np.array([10, 20, 30, 40, 50]);
 
 plt.subplot(1, 2, 1);
-plt.plot(x, y, linestyle=:);
+plt.plot(x, y, linestyle=:"");
 
 x = np.array([50, 60, 70, 80, 90, 100]);
 y = np.array([10, 40, 20, 30, 50, 60]);
