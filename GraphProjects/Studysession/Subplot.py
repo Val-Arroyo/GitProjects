45ae52f18a71e56408e2c644e44effa6484af3b3
valarroyo-gif <valarroyo39@gmail.com>
2023-08-21 15:40:54 +1200

    Update Subplot.py

--- GraphProjects/Studysession/Subplot.py
+++ GraphProjects/Studysession/Subplot.py
@@ -5,12 +5,12 @@ x = np.array([1, 2, 3, 4, 5]);
 y = np.array([10, 20, 30, 40, 50]);
 
 plt.subplot(1, 2, 1);
-plt.plot(x, y, linestyle="Dashed");
+plt.plot(x, y, linestyle="dashed");
 
 x = np.array([50, 60, 70, 80, 90, 100]);
 y = np.array([10, 40, 20, 30, 50, 60]);
 
 plt.subplot(1, 2, 2);
-plt.plot(x, y, linestyle="Dashed");
+plt.plot(x, y, linestyle="dashed");
 
 plt.show();
