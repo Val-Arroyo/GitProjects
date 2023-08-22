6b0d74d78671f6f73aff79a5ca602e8864f47665
valarroyo-gif <valarroyo39@gmail.com>
2023-08-22 16:41:29 +1200

    Update Sample.py

--- GraphProjects/Studysession/Sample.py
+++ GraphProjects/Studysession/Sample.py
@@ -3,7 +3,7 @@ import numpy as np
 
 pie_array = np.array([45, 25, 20, 10]);
 anime = ["One Piece", "Naruto", "Bleach","Dragon Ball"];
-myexplode = [0.2.];
+myexplode = [0.2, 0.2];
 
 plt.pie(pie_array, anime="Anime");
 
