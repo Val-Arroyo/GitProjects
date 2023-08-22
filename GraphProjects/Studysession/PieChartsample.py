77923e1752cc3c645c9665d1cec8bd9b2be7aca0
valarroyo-gif <valarroyo39@gmail.com>
2023-08-22 15:11:28 +1200

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.array([25, 10, 15, 50]);
+y = np.array([50, 25, 15, 10]);
+mylabels = ["A", "B", "C", "D"];
+myexplode = [0.2, 0.2, 0.2, 0.2];
 
+plt.pie(y, labels= mylabels, explode=myexplode, shadow= True);
+plt.legend(title = "Grades");
+plt.show();
