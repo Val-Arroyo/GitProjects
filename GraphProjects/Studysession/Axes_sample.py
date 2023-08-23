26222f7b3d74abb0928735ce0491ac78bcae84d1
valarroyo-gif <valarroyo39@gmail.com>
2023-08-23 17:29:47 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -3,10 +3,11 @@ import numpy as np
 
 x = [2000, 2005, 2010, 2015, 2020];
 y = [10000, 20000, 30000, 40000, 50000];
-z =
+z = [1000000, 2000000, 3000000, 4000000, 5000000]
 
-fig, ax = plt.subplots();
+fig, ax = plt.subplots(2);
 
-ax.plot(x, y);
+ax[1].plot(x, y);
+
 
 plt.show();
