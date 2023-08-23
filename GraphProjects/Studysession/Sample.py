d2da526f897c3c934f37387038e83ff260068917
valarroyo-gif <valarroyo39@gmail.com>
2023-08-23 17:01:59 +1200

    Update Sample.py

--- GraphProjects/Studysession/Sample.py
+++ GraphProjects/Studysession/Sample.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-years = [1995, 2000, 2005, 2010, 2015];
+years = [1995, 2000, 2005, 2010, 2015, 2020];
 One_Piece = [];
 Naruto = [];
 Bleach = [];
