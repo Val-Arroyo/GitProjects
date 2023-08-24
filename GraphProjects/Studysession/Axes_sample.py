826869998533e4f8d47816a788973c4c29525ad5
valarroyo-gif <valarroyo39@gmail.com>
2023-08-24 20:00:53 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -7,7 +7,7 @@ Naruto = np.array([10, 30, 40, 50, 60, 70, 90]);
 
 fig, ax = plt.subplots();
 
-ax.plot(year, One_piece, title=);
+ax.plot(year, One_piece, title='One Piece');
 
 ax.plot(year, Naruto);
 
