467627ec1405fb77e574f5993ca56f1cc211fee9
valarroyo-gif <valarroyo39@gmail.com>
2023-08-24 20:07:47 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -7,8 +7,8 @@ Naruto = np.array([10, 30, 40, 50, 60, 70, 90]);
 
 fig, ax = plt.subplots();
 
-ax.plot(year, One_piece, marker='o');
-ax.plot(year, Naruto, marker='o');
+ax.plot(year, One_piece, marker='o', label='One Piece');
+ax.plot(year, Naruto, marker='o', label=);
 
 plt.title("Anime Popularity Chart");
 plt.ylabel("Popularity Chart");
