9151b4093d6f3805590c0f6033bddba8fae31ad4
valarroyo-gif <valarroyo39@gmail.com>
2023-08-24 19:59:09 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 year = np.array([1995, 2000, 2005, 2010, 2015, 2020, 2025]);
-One_piece = np.array([20, 30, 50, 40, 60]);
+One_piece = np.array([20, 30, 50, 40, 60, 80]);
 Naruto = np.array([]);
 
 fig, ax = plt.subplots();
