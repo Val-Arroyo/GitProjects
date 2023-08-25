be2f3a190f1fb172d736f7a362a666842412501a
valarroyo-gif <valarroyo39@gmail.com>
2023-08-25 21:14:18 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -6,6 +6,7 @@ One_piece = np.array([20, 30, 50, 40, 60, 80, 100]);
 Naruto = np.array([10, 30, 40, 50, 60, 70, 90]);
 Bleach = np.array([0, 20, 40, 30, 65, 85, 95,]);
 
+
 fig, ax = plt.subplots();
 
 ax.plot(year, One_piece, marker='o', label='One Piece');
