73d415b5f9f586ad17093baa63ae9203616be530
valarroyo-gif <valarroyo39@gmail.com>
2023-08-25 21:23:33 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -12,7 +12,7 @@ fig, ax = plt.subplots();
 ax.plot(year, One_piece, marker='o', label='One Piece', linewidth=2);
 ax.plot(year, Naruto, marker='o', label='Naruto', linewidth=2);
 ax.plot(year, Bleach, marker='o', label='Bleach', linewidth=2);
-ax.plot(year, DragonBall, marker='o', label='Dragon Ball');
+ax.plot(year, DragonBall, marker='o', label='Dragon Ball', linewidth=);
 
 plt.title("Anime Popularity Chart");
 plt.ylabel("Popularity Chart");
