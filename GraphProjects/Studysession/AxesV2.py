442cd66cacd5b65557aa2f2e77e3afe980be1aa6
valarroyo-gif <valarroyo39@gmail.com>
2023-08-26 19:46:40 +1200

    Update AxesV2.py

--- GraphProjects/Studysession/AxesV2.py
+++ GraphProjects/Studysession/AxesV2.py
@@ -2,17 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 year = np.array([1995, 2000, 2005, 2010, 2015, 2020, 2025]);
-One_piece = np.array([]);
-Naruto = np.array([]);
-Bleach = np.array([]);
-DragonBall = np.array([]);
+One_piece = np.array([20, 30, 50, 40, 60, 80, 100]);
+Naruto = np.array([10, 30, 40, 50, 60, 70, 90]);
+Bleach = np.array([0, 20, 40, 30, 65, 85, 95,]);
+DragonBall = np.array([90, 100, 80, 75, 65, 40, 90]);
 
 fig, ax = plt.subplots(2, 2);
 
+ax[0, 0].plot(year, One_piece, color='red');
+ax[0, 1].plot(year, Naruto, color='blue');
+ax[1, 0].plot(year, Bleach, color='green');
+ax[1, 1].plot(year, DragonBall, color='orange');
 
-plt.title();
-plt.xlabel();
-plt.ylabel();
-plt.grid();
+ax[0, 0].title.set_text("One Piece");
+ax[0, 1].title.set_text("Naruto");
+ax[1, 0].title.set_text("Bleach");
+ax[1, 1].title.set_text("Dragon Ball");
+
 plt.show();
 
