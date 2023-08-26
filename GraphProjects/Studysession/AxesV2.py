89d431e094548b52de8f2343d723fce1284c66fc
valarroyo-gif <valarroyo39@gmail.com>
2023-08-26 19:35:14 +1200

    Update AxesV2.py

--- GraphProjects/Studysession/AxesV2.py
+++ GraphProjects/Studysession/AxesV2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-year = np.array([1995, 2000, 2005, 2010, 2015]);
+year = np.array([1995, 2000, 2005, 2010, 2015, 2020, 2025]);
 One_piece = np.array([]);
 Naruto = np.array([]);
 Bleach = np.array([]);
