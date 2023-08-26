6ed1d40ed1967dfa0af905cccfc4613f6209774b
valarroyo-gif <valarroyo39@gmail.com>
2023-08-26 19:34:45 +1200

    Update AxesV2.py

--- GraphProjects/Studysession/AxesV2.py
+++ GraphProjects/Studysession/AxesV2.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+year = np.array([]);
 One_piece = np.array([]);
 Naruto = np.array([]);
 Bleach = np.array([]);
 DragonBall = np.array([]);
+
+fig, ax = plt.subplots(2, 2);
 
