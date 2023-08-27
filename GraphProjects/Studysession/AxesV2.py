e3742afdd7ba52cc12bc55898f36455a4065c69c
valarroyo-gif <valarroyo39@gmail.com>
2023-08-27 20:05:39 +1200

    Update AxesV2.py

--- GraphProjects/Studysession/AxesV2.py
+++ GraphProjects/Studysession/AxesV2.py
@@ -12,7 +12,7 @@ fig, ax = plt.subplots(2, 2);
 ax[0, 0].plot(year, One_piece, color='red', marker="o");
 ax[0, 1].plot(year, Naruto, color='blue', marker="o");
 ax[1, 0].plot(year, Bleach, color='green', marker="o");
-ax[1, 1].plot(year, DragonBall, color='orange', marker="");
+ax[1, 1].plot(year, DragonBall, color='orange', marker="o");
 
 ax[0, 0].title.set_text("One Piece");
 ax[0, 1].title.set_text("Naruto");
