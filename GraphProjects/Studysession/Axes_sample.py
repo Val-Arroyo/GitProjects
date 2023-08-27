6da89004629b13604560bd29509adc6b39fb2d77
valarroyo-gif <valarroyo39@gmail.com>
2023-08-27 19:49:34 +1200

    Update Axes_sample.py

--- GraphProjects/Studysession/Axes_sample.py
+++ GraphProjects/Studysession/Axes_sample.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 import numpy as np
 
 year = np.array([1995, 2000, 2005, 2010, 2015, 2020, 2025]);
@@ -8,6 +9,7 @@ Bleach = np.array([0, 20, 40, 30, 65, 85, 95,]);
 DragonBall = np.array([90, 100, 80, 75, 65, 40, 90]);
 
 fig, ax = plt.subplots();
+
 
 ax.plot(year, One_piece, marker='o', label='One Piece', linewidth=2);
 ax.plot(year, Naruto, marker='o', label='Naruto', linewidth=2);
