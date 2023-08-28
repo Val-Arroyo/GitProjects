9d2e4debdf2f97a6381ae1c3895be71376aa08b4
valarroyo-gif <valarroyo39@gmail.com>
2023-08-28 16:01:49 +1200

    Update Barplot.py

--- GraphProjects/Studysession/Barplot.py
+++ GraphProjects/Studysession/Barplot.py
@@ -12,4 +12,4 @@ plt.bar(courses, values, color='Maroon', width = 0.4);
 
 plt.xlabel("Courses Offered");
 plt.ylabel("No. of Students Offered");
-plt.title("Student Enrolled ");
+plt.title("Student Enrolled In ");
