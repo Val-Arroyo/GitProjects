a32b59c98ad02c3c25c4fdd83c719fb878b46ce9
valarroyo-gif <valarroyo39@gmail.com>
2023-08-28 16:04:37 +1200

    Update Barplot.py

--- GraphProjects/Studysession/Barplot.py
+++ GraphProjects/Studysession/Barplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = {'C': 20, 'C++': 15, 'Java': 30, 'Python': 35};
+data = {'C': 40, 'C++': 15, 'Java': 30, 'Python': 35};
 
 courses = list(data.keys());
 values = list(data.keys());
