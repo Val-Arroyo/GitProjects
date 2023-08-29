f9b232c181bfd7f5110745cb89ba36a40f3f06be
valarroyo-gif <valarroyo39@gmail.com>
2023-08-29 15:10:52 +1200

    Update Barplot.py

--- GraphProjects/Studysession/Barplot.py
+++ GraphProjects/Studysession/Barplot.py
@@ -1,17 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-data = {'C':20, 'C++': 15, 'Java': 30, 'Python': 35};
-
-courses = list(data.keys());
-values = list(data.values());
-
-fig = plt.figure(figsize = (10, 5));
-
-plt.bar(courses, values, color='Maroon', width = 0.4);
-
-plt.xlabel("Courses Offered");
-plt.ylabel("No. of Students Offered");
-plt.title("Student Enrolled In Different Courses");
-
-plt.show();
+\
