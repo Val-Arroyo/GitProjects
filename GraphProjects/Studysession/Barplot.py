f7e13f436c6ff9781c0bf902fbb2e8a3fcc6c432
valarroyo-gif <valarroyo39@gmail.com>
2023-08-29 15:14:34 +1200

    Update Barplot.py

--- GraphProjects/Studysession/Barplot.py
+++ GraphProjects/Studysession/Barplot.py
@@ -4,4 +4,4 @@ import numpy as np
 data = np.array['One Piece': 80, 'Naruto': 25, 'Bleach': 20, 'Dragon Ball':50 ];
 
 anime = list(data.keys());
-values = list(data.)
+values = list(data.values());
