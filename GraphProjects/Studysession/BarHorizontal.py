0e66d76bf697e4c2b3db2eb4159e61ed2dda4e36
valarroyo-gif <valarroyo39@gmail.com>
2023-08-30 16:25:44 +1200

    Update BarHorizontal.py

--- GraphProjects/Studysession/BarHorizontal.py
+++ GraphProjects/Studysession/BarHorizontal.py
@@ -3,7 +3,7 @@ import numpy as np
 
 x = ['Java', 'Python', 'C', 'C++', 'Javascript'];
 y1 = [10, 60, 30, 20, 40];
-y2 = [20, 120, 60, 40, 80];
+y2 = [20, 100, 60, 40, 80];
 
 plt.bar(x, y1, color = 'red');
 plt.bar(x, y2, bottom = y2, color = 'blue');
