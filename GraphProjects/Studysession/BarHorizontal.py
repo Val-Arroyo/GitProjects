c67e2e9ae2b637628d3f94af3ed200d1d27e5399
valarroyo-gif <valarroyo39@gmail.com>
2023-08-30 16:15:24 +1200

    Update BarHorizontal.py

--- GraphProjects/Studysession/BarHorizontal.py
+++ GraphProjects/Studysession/BarHorizontal.py
@@ -4,7 +4,7 @@ import numpy as np
 y = ['One', 'Two', 'Three', 'Four', 'Five'];
 x = [15, 25, 45, 10, 50];
 
-plt.barh(y, x, color = 'Maroon', width = 0.4);
+plt.barh(y, x, color = 'Maroon', linewidth = );
 
 plt.xlabel("Price");
 plt.ylabel("Pen Sold");
