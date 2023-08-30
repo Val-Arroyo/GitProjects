3a7f43c0abcf7b345ea527bcd03473cc56b76dba
valarroyo-gif <valarroyo39@gmail.com>
2023-08-30 16:29:57 +1200

    Update BarHorizontal.py

--- GraphProjects/Studysession/BarHorizontal.py
+++ GraphProjects/Studysession/BarHorizontal.py
@@ -6,5 +6,5 @@ y1 = [10, 20, 10, 20, 30];
 y2 = [15, 25, 15, 25, 35];
 
 plt.bar(x, y1, color = 'r');
-plt.bar(x, y2, bottom = y2, color = 'blue');
+plt.bar(x, y2, bottom = y2, color = 'b');
 plt.show();
