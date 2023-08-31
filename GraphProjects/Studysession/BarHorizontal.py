a8bdf8134427aaf9032ec2b42352ff5c5f3a84e7
valarroyo-gif <valarroyo39@gmail.com>
2023-08-31 15:36:26 +1200

    Update BarHorizontal.py

--- GraphProjects/Studysession/BarHorizontal.py
+++ GraphProjects/Studysession/BarHorizontal.py
@@ -12,7 +12,7 @@ plt.bar(anime, y2, bottom = y1, color = 'red');
 
 plt.xlabel("Anime List ");
 plt.ylabel("Popularity Chart");
-plt.legend(["One Piece"]);
+plt.legend(["Release Bar", "Current Bar"]);
 plt.title("Anime Popularity");
 
 plt.show();
