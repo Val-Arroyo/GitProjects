a8463b69698b7baae85928ab26b7a45b95ca8d2a
valarroyo-gif <valarroyo39@gmail.com>
2023-08-31 15:15:06 +1200

    Update BarHorizontal.py

--- GraphProjects/Studysession/BarHorizontal.py
+++ GraphProjects/Studysession/BarHorizontal.py
@@ -1,4 +1,4 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-anime = ['One Piece','Naruto Shippuden','Bleach', '', ''];
+anime = ['One Piece','Naruto Shippuden','Bleach', 'Dragon Ball', ''];
