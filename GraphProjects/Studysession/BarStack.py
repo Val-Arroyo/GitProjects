ba523330f1b2139b1863b2c311011e2acb4ac18c
valarroyo-gif <valarroyo39@gmail.com>
2023-09-01 22:52:05 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -3,11 +3,11 @@ import numpy as np
 
 anime = ['One Piece', 'Naruto Shippuden','Bleach','Dragon Ball'];
 
-y1 = [20, 40, 15, 50];
-y2 = [100, 80, 30, 30];
+y1 = np.array([20, 40, 15, 50]);
+y2 = np.array([100, 80, 30, 30]);
 
 plt.bar(anime, y1, color = 'blue');
-plt.bar(anime, bottom = y1, color = 'red');
+plt.bar(anime, y2, bottom = y1, color = 'red');
 
 plt.title("Anime Popularity Chart Before and After");
 plt.xlabel("Anime List");
