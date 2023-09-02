f1b192d5e2f1fefecfe65a9b208917dfcd43f97b
valarroyo-gif <valarroyo39@gmail.com>
2023-09-02 20:18:25 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-bleach = ["Ichigo", "Uryu Ishida", "Chad", "Inoue"];
+bleach = ["Ichigo", "Uryu", "Chad", "Inoue"];
 
 y1 = np.array([40, 30, 20, 10]);
 y2 = np.array([100, 90, 50, 40]);
 
-plt.plot(bleach, y1, color = 'red', width = 0.4);
+plt.plot(bleach, y1, color = 'red');
 plt.plot(bleach, y2, bottom = y1, color = 'blue', width = 0.4);
 
 plt.xlabel("Character Name");
