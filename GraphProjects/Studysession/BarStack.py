090fdc62f1a414541fcd44d1c5de2e30d3702b16
valarroyo-gif <valarroyo39@gmail.com>
2023-09-02 20:15:39 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -4,7 +4,7 @@ import numpy as np
 bleach = ["Ichigo", "Uryu Ishida", "Chad", "Inoue"];
 
 y1 = np.array([40, 30, 20, 10]);
-y2 = np.array([100, 90, 50]);
+y2 = np.array([100, 90, 50,]);
 
 plt.plot(bleach, y1, color = 'red', width = 0.4);
 plt.plot(bleach, y2, bottom = y1, color = 'blue', width = 0.4);
