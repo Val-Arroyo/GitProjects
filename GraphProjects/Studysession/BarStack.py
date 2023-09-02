a5cabbfd2608ed636e6af30c72609f42f67a19be
valarroyo-gif <valarroyo39@gmail.com>
2023-09-02 20:19:02 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -7,7 +7,7 @@ y1 = np.array([40, 30, 20, 10]);
 y2 = np.array([100, 90, 50, 40]);
 
 plt.plot(bleach, y1, color = 'red');
-plt.plot(bleach, y2, bottom = y1, color = 'blue');
+plt.plot(bleach, y2, bottom = y1);
 
 plt.xlabel("Character Name");
 plt.ylabel("Strength Level");
