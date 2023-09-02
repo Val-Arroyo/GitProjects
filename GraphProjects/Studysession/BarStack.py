a924693db1f10cb45260ea2c2bbfca919ebb38cf
valarroyo-gif <valarroyo39@gmail.com>
2023-09-02 20:20:13 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -6,7 +6,7 @@ bleach = ["Ichigo", "Uryu", "Chad", "Inoue"];
 y1 = np.array([40, 30, 20, 10]);
 y2 = np.array([100, 90, 50, 40]);
 
-plt.bar(bleach, y1, color = 'red');
+plt.bar(bleach, y1, color = 'red', width = 0.4);
 plt.bar(bleach, y2, bottom = y1, color = 'blue');
 
 plt.xlabel("Character Name");
