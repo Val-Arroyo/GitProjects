ecadcfcc551177fd73b58811e73cdee3ead1af65
valarroyo-gif <valarroyo39@gmail.com>
2023-09-03 13:25:20 +1200

    Update BarStack.py

--- GraphProjects/Studysession/BarStack.py
+++ GraphProjects/Studysession/BarStack.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-bleach = ["Ichigo", "Uryu", "Chad", "Inoue"];
+bleach = ["Ichigo Kurosaki", "Uryu Ishida", "Chad", "Inoue"];
 
 y1 = np.array([40, 30, 20, 10]);
 y2 = np.array([100, 90, 50, 40]);
 
-plt.bar(bleach, y1, color = 'red', width = 0.4);
-plt.bar(bleach, y2, bottom = y1, color = 'blue', width = 0.4);
+plt.bar(bleach, y1, color = 'red', width = 0.2);
+plt.bar(bleach, y2, bottom = y1, color = 'blue', width = 0.2);
 
+
+plt.grid();
 plt.xlabel("Character Name");
 plt.ylabel("Strength Level");
 plt.title("Bleach Main Character Strength Levels per Arc");
