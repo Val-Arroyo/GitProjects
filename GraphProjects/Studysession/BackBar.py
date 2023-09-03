27600988773dbf7002e56de531a1aee53a96e4cb
valarroyo-gif <valarroyo39@gmail.com>
2023-09-03 13:39:21 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -3,7 +3,7 @@ import numpy as np
 
 A = np.array([4, 2, 3, 6, 1, 5]);
 B = np.array([2, 4, 6, 3, 2, 5]);
-X = np.arrange(6);
+X = np.arange(6);
 
 plt.barh(X, A, color = 'red');
 plt.barh(X,  -B, color = 'blue');
