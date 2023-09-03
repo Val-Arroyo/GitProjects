784cd3d0db360d150c3230d4cd5d000032670eca
valarroyo-gif <valarroyo39@gmail.com>
2023-09-03 13:42:45 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -6,7 +6,7 @@ B = np.array([2, 4, 6, 3, 2, 5]);
 X = np.arange(6);
 
 plt.barh(X, A, color = 'red', linewidth = 0.4);
-plt.barh(X,  -B, color = 'blue', linewidth = 0.1);
+plt.barh(X,  -B, color = 'blue', linewidth = 0.4);
 plt.title("Back To Back Graph Sample");
 
 plt.grid();
