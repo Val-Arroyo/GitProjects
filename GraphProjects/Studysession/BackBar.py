6480f97db16eef896b0645bc377ad4db990aa4e8
valarroyo-gif <valarroyo39@gmail.com>
2023-09-04 15:39:05 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -5,7 +5,7 @@ A = np.array([40, 30, 20, 10, 60, 70, 50, 80]);
 B = np.array([80, 60, 40, 20, 120, 140, 100, 160]);
 X = np.arange(8);
 
-plt.barh(X, A, color = 'red', alpha = 0.4, align = 'center');
+plt.barh(X, A, color = 'red', alpha = 0.8, align = 'center');
 plt.barh(X, -B, color = 'blue', alpha = 0.4, align = 'center');
 
 plt.grid();
