6dea507d5144cf9505cf14d8cf70a0e6dfd7f6f2
valarroyo-gif <valarroyo39@gmail.com>
2023-09-04 15:36:32 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 A = np.array([40, 30, 20, 10, 60, 70, 50, 80]);
-B = np.array([80, 60]);
+B = np.array([80, 60, 40]);
 X = np.arange(8);
 
 plt.barh();
