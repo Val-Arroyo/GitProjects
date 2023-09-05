86404d617e196198ea1fbd7c4ee9af5adeca533a
valarroyo-gif <valarroyo39@gmail.com>
2023-09-05 15:47:00 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -1,12 +1,1 @@
 import matplotlib.pyplot as plt
-import numpy as  np
-
-x = ["A", "B", "C", "D"];
-y = [1, 2, 3, 4];
-
-plt.barh(x, y);
-
-for index, value in enumerate(y):
-    plt.text(value, index, str(value));
-
-plt.show();
