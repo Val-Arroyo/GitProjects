dd368a408ab7e9fbf95c924bf2b97193920b0998
valarroyo-gif <valarroyo39@gmail.com>
2023-09-05 15:09:59 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-A = np.array([40, 30, 20, 10, 60, 70, 50, 80]);
-B = np.array([80, 60, 40, 20, 120, 140, 100, 160]);
-X = np.arange(8);
+A = np.array([10, 20, 30, 40, 50, 60, 70, 80])
+B = np.array([20, 40, 60, 80, 100, 120, 140, 160])
+X = np.arange(8)
 
-plt.barh(X, B, color = 'red', alpha = 0.8, align = 'center');
-plt.barh(X, -A, color = 'blue', alpha = 0.8, align = 'center');
+plt.barh(X, B, color = 'red', alpha = 0.8,);
+plt.barh(X, -A, color = 'blue', alpha = 0.8 );
 
 plt.grid();
-plt.xlabel("Numbers");
-plt.ylabel("Numbers");
+plt.xlabel("Positive and Negative Label", size = 10);
+plt.ylabel("Range Label", size = 10);
 plt.title("Graph Sample");
 plt.show();
