f0ede6bf13a8315f20607ef9b610c792380bd678
valarroyo-gif <valarroyo39@gmail.com>
2023-09-07 15:47:16 +1200

    Update BackBar.py

--- GraphProjects/Studysession/BackBar.py
+++ GraphProjects/Studysession/BackBar.py
@@ -3,16 +3,16 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-A = np.array([20, 60]);
-B = np.array([]);
+A = np.array([20, 60, 40, 80, 120, 100, 160, 140]);
+B = np.array([10, 30, 20, 40, 60, 50, 80, 70 ]);
 X = np.arange(8);
 
-plt.bar();
-plt.bar();
+plt.bar(X, A, color = 'maroon', alpha = 0.8);
+plt.bar(X, -B, color = 'red', alpha = 0.8);
 
 plt.grid();
-plt.title();
-plt.xlabel();
-plt.ylabel();
+plt.title("Positive and Negative Graph");
+plt.xlabel("Numbers Display");
+plt.ylabel("Positive and Negative Number Display");
 
 plt.show();
