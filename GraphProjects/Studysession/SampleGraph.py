1dbd1f3b10ab2b5fc7d4edd9337e315984d1da9f
valarroyo-gif <valarroyo39@gmail.com>
2023-09-08 20:55:20 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -4,14 +4,3 @@ from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 import seaborn as sns
 import pandas as pd
-
-a = np.array([22, 87, 5, 43, 56,
-              73, 55, 54, 11,
-              20, 51, 5, 79, 31,
-              27]);
-
-fig, ax = plt.subplots(figsize = (10, 7));
-
-ax.hist(a, bins = [0, 25, 50, 75, 100]);
-
-plt.show();
