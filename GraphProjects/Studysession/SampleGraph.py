0561797f532917ed04a8e851b16354fda31a7174
valarroyo-gif <valarroyo39@gmail.com>
2023-09-09 14:42:10 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -11,7 +11,7 @@ a = np.array([22, 87, 5, 43, 56,
               20, 51, 5, 79, 31,
               27]);
 
-fig, ax = plt.subplots(figsize = (8, 5));
+fig, ax = plt.subplots(figsize = (10, 10));
 
 ax.hist(a, bins = [0, 25, 50, 75, 100]);
 
