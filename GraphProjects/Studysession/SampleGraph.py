e709bf5d032da411846a9a543ccfaaac91b27e00
valarroyo-gif <valarroyo39@gmail.com>
2023-09-09 14:39:35 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -6,4 +6,7 @@ import numpy as np
 import seaborn as sns   
 import pandas as np
 
-a = np.array([]);
+a = np.array([22, 87, 5, 43, 56,
+              73, 55, 54, 11,
+              20, 51, 5, 79, 31,
+              27]);
