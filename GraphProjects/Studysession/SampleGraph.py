063f63f7565c19b774dea8f77abd15ed9d0ec952
valarroyo-gif <valarroyo39@gmail.com>
2023-09-09 14:36:34 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from matplotlib.ticker import PercentFormatter
-
+from matplotlib
 import numpy as np
 import seaborn as sns   
 import pandas as np
