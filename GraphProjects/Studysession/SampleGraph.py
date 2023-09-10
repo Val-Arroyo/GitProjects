148ab926e5be611299563930ad5300bf75b2c1ff
valarroyo-gif <valarroyo39@gmail.com>
2023-09-10 19:10:06 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -5,4 +5,7 @@ from matplotlib import colors
 import seaborn as sns
 import pandas as pd
 
-a = np.array([]);
+a = np.array([22, 87, 5, 43, 56,
+              73, 55, 54, 11,
+              20, 51, 5, 79, 31,
+              27]);
