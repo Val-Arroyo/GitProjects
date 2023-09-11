e173e3e95049fa44a7e8c12683d5e31f43b4b0b6
valarroyo-gif <valarroyo39@gmail.com>
2023-09-11 15:48:25 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors
-from matplotlib import 
+from matplotlib.ticker import 
 import numpy as np
 import seaborn as sns
 import pandas as pd
