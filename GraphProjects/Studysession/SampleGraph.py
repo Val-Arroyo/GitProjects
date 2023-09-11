fc4e7481748de33f6db2844948a8c714bde00d00
valarroyo-gif <valarroyo39@gmail.com>
2023-09-11 16:05:50 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -4,7 +4,7 @@ import numpy as np
 series1 = np.random.randn(500, 1);
 series2 = np.random.randn(400, 1);  
 
-plt.hist(series1);
+plt.hist(series1, color = 'red');
 plt.hist(series2);
 
 plt.show()
