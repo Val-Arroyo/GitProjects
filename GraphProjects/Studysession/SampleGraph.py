187e2f15ef127432d36f28e49c4410b519c842c3
valarroyo-gif <valarroyo39@gmail.com>
2023-09-11 15:59:49 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -5,10 +5,13 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-a = np.array([20, 15, 30, 40, 10, 25]);
+a = np.array([22, 87, 5, 43, 56,
+              73, 55, 54, 11,
+              20, 51, 5, 79, 31,
+              27]);
 
 fig, ax = plt.subplots(figsize = (10, 7));
 
-ax.hist(a, bins = [0, 20, 30,, 40, 50]);
+ax.hist(a, bins = [0, 25, 50, 75, 100]);
 
 plt.show();
