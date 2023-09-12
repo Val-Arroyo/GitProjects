b1919bb2f0263e292d361f85ec92e41247d73229
valarroyo-gif <valarroyo39@gmail.com>
2023-09-12 15:21:59 +1200

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.lib.histograms import histogram
 
 series1 = np.random.randn(500, 1);
 series2 = np.random.randn(400, 1);  
 
-plt.hist(series1, color = 'red');
-plt.hist(series2, color = 'blue');
 
+plt.hist(series1, label='series1', alpha=.8, edgecolor='red')
+ 
+
+plt.hist(series2, label='series2', alpha=0.7, edgecolor='yellow')
+plt.legend()
+
+plt.show()
 plt.show()
