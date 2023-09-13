0afc51bb1da11fa0b4b0ccaf9c110711b1d15c61
valarroyo-gif <valarroyo39@gmail.com>
2023-09-13 16:04:07 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.random.normal(170, 10, 250)
+random_number = np.random.normal(170, 10, 250);
 
-plt.hist(x)
-plt.show() 
+print(random_number);
+plt.hist(random_number)
+plt.show()
