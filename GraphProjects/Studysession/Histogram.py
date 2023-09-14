3dc5a4f2474d539dcab12d4ccb2e3f2cd2d4cf97
valarroyo-gif <valarroyo39@gmail.com>
2023-09-14 16:30:27 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -8,6 +8,7 @@ random_number = np.random.normal(170, 10, 250);
 print(random_number);
 plt.hist(random_number, color='maroon');
 
+plt.grid();
 plt.title("Histogram Random Graph");
 plt.legend();
 plt.show()
