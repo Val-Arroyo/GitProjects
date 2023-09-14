b50800a41123d9521b7f8e1394b23a049061e9b4
valarroyo-gif <valarroyo39@gmail.com>
2023-09-14 15:16:47 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -1,12 +1,1 @@
-import matplotlib.pyplot as plt
-from matplotlib import colors
-from matplotlib.ticker import PercentFormatter
-import numpy as np
-import pandas as pd
-import seaborn as sns
-
-random_number = np.random.normal(170, 10, 250);
-
-print(random_number);
-plt.hist(random_number);
-plt.show();
+import 
