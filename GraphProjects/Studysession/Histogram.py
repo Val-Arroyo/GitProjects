9a443834425e9c1da7b4b9dc406f4bef8e6749a2
valarroyo-gif <valarroyo39@gmail.com>
2023-09-17 19:51:11 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-rand_number = np.random.normal(500, 1);
+rand_number = np.random.randn(500, 1);
 
 plt.grid();
 plt.hist(rand_number, color = 'red', edgecolor = 'blue');
