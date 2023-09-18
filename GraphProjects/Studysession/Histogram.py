5489676763824bed5072b77ed56e924d75a6dc65
valarroyo-gif <valarroyo39@gmail.com>
2023-09-18 15:44:31 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5, 6, 7, 4] 
 bins = [1, 2, 3, 4, 5, 6, 7]
 
-fig = plt.figure(figsize = (5, 5));
+fig = plt.figure(figsize = (8, 5));
 
 plt.hist(x, bins);
 
