6f7828ce677d7e3c660a8e28d506e12a2791ce8e
valarroyo-gif <valarroyo39@gmail.com>
2023-09-18 15:42:45 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -2,11 +2,3 @@ import matplotlib as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-
-x = [1, 2, 3, 4, 5, 6, 7, 4];
-bins = [1, 2, 3, 4, 5, 6, 7];
-
-plt.hist(x, bins);
-plt.title("Histogram");
-plt.legend();
-plt.show();
