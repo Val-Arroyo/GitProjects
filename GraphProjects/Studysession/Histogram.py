4e1c5a408e6e1298090ea9829a8a69b260907635
valarroyo-gif <valarroyo39@gmail.com>
2023-09-18 15:30:43 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -5,7 +5,7 @@ import pandas as pd
 from numpy.lib.histograms import histogram
 
 rand_number = np.random.randn(500, 1);
-fig = plt.figure(figsize=(8, 3));
+fig = plt.figure(figsize=(8, 4));
 
 plt.grid();
 plt.hist(rand_number, color = 'red', edgecolor = 'blue', label = 'Histogram');
