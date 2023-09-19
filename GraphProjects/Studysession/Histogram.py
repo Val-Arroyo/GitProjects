650b161df227f04e5ebe70ae7d3f3213514daac3
valarroyo-gif <valarroyo39@gmail.com>
2023-09-19 15:35:21 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -10,4 +10,6 @@ n_bins = 20;
 x = np.random.randn(N_points);
 y = .8 ** x + np.random.randn(10000) + 25
 
-fig, axs = plt.subplots(1, 1, figsize = (10, 7).)
+fig, axs = plt.subplots(1, 1, 
+                        figsize = (10, 7),
+                        tight_layout = True );
