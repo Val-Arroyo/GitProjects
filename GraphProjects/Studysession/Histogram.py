9d8d9c99c863538ba1ac2170d6491ea2a1c0111e
valarroyo-gif <valarroyo39@gmail.com>
2023-09-19 15:49:25 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -14,7 +14,7 @@ fig, axs = plt.subplots(1, 1,
                         figsize = (7, 5),
                         tight_layout = True );
 
-axs.hist(x, bins = n_bins, color = 'red', linestyle = "-");
+axs.hist(x, bins = n_bins, color = 'red', linestyle = "-.");
 axs.grid(color = 'red');
 
 plt.show();
