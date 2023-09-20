ea957df6453ca42585d6e7c4ec3c5162a6b2254b
valarroyo-gif <valarroyo39@gmail.com>
2023-09-20 17:46:31 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -3,14 +3,17 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib import colors
+from numpy.lib.histograms import histogram 
 
 graph1 = np.random.randn(500, 1);
 graph2 = np.random.randn(400, 1);
 graph3 = np.random.randn(300, 1);
 
+n_bins = 14
+
 fig, axs = plt.subplots(1, 1, figsize = (7, 5));
 
-axs.hist(graph1, color = 'red', edgecolo = 'black');
+axs.hist(graph1, color = 'red', edgecolor = 'black');
 axs.hist(graph2, color = 'blue', edgecolor = 'black');
 axs.hist(graph3, color = 'green', edgecolor = 'black');
 
