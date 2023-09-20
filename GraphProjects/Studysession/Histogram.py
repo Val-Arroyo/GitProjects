389876ff1d37d7d69ff8b587e677d1ab50e7673f
valarroyo-gif <valarroyo39@gmail.com>
2023-09-20 17:43:50 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -4,9 +4,9 @@ import seaborn as sns
 import pandas as pd
 from matplotlib import colors
 
-graph1 = np.random.randn(300, 1);
+graph1 = np.random.randn(500, 1);
 graph2 = np.random.randn(400, 1);
-graph3 = np.random.randn(500, 1);
+graph3 = np.random.randn(300, 1);
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5));
 
