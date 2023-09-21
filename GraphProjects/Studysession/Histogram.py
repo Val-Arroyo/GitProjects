b0cb1c536d874eac64cae4143a1dcac15bb1cc04
valarroyo-gif <valarroyo39@gmail.com>
2023-09-21 15:51:18 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -8,7 +8,7 @@ graph_2 = np.random.randn(50, 1);
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5));
 
-axs.hist(graph_1);
+axs.hist(graph_1, color = 'red');
 axs.hist(graph_2);
 
 plt.grid(color = 'red', linestyle = '-.', linewidth = 0.6);
