abaa35440c80d6e4e9e5e8f56137fa7d683606d2
valarroyo-gif <valarroyo39@gmail.com>
2023-09-21 15:58:25 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -5,8 +5,8 @@ import pandas as pd
 from matplotlib import colors 
 from numpy.lib.histograms import histogram
 
-graph_1 = np.random.randn(100, 1);
-graph_2 = np.random.randn(50, 1);
+graph_1 = np.random.randn(1000, 1);
+graph_2 = np.random.randn(500, 1);
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5));
 
