8f1aa6e8b7fd542c7e910ac3a02ee7944ea05da5
valarroyo-gif <valarroyo39@gmail.com>
2023-09-22 21:39:29 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -7,5 +7,5 @@ from matplotlib import colors
 from numpy.lib.histograms import histogram
 from matplotlib.ticker import PercentFormatter
 
-Hist_1 = np.random.randn();
+Hist_1 = np.random.randn(500, 1);
 Hist_2 = np.random.randn();
