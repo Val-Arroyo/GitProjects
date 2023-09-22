78b81ee1115adaa0fda13ddb55320d59882df045
valarroyo-gif <valarroyo39@gmail.com>
2023-09-22 21:37:22 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-import 
+import matplotlib.ticker as mtick
 from matplotlib import colors
 from numpy.lib.histograms import histogram
