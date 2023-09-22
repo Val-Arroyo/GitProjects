ccd29069e59fc86526ca0bc3e7ca241219d2bcd6
valarroyo-gif <valarroyo39@gmail.com>
2023-09-22 21:40:11 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -10,4 +10,4 @@ from matplotlib.ticker import PercentFormatter
 Hist_1 = np.random.randn(500, 1);
 Hist_2 = np.random.randn(300, 1);
 
-fig, axs = plt.subplots(1, 1, figsize = (7, 5))
+fig, axs = plt.subplots(1, 1, figsize = (7, 5));
