179dbc31bdfa6c8a76f2986105b37ffa83d76b15
valarroyo-gif <valarroyo39@gmail.com>
2023-09-23 15:06:49 +1200

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-
+from matplotlib
 from matplotlib.ticker import PercentFormatter
 from numpy.lib.histograms import histogram
