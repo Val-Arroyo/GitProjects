f0e9250df2ed5ed9b8ccde316d08a8037f9bedb5
valarroyo-gif <valarroyo39@gmail.com>
2023-09-25 15:52:46 +1300

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -16,7 +16,7 @@ axs.hist(Hist_2, color = 'blue', edgecolor = 'black');
 
 plt.grid(color = 'red', linestyle = '-.', linewidth = 0.5);
 plt.legend();
-plt.xlabel();
+plt.xlabel("X label Numbers");
 plt.ylabel();
 
 plt.show();
