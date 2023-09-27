dd47a0c0f2c8b24d8be767302d986a943c8e0e11
valarroyo-gif <valarroyo39@gmail.com>
2023-09-27 20:20:25 +1300

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -11,7 +11,7 @@ random_numner_2 = np.random.normal(140, 10, 240);
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
 
-axs.hist();
+axs.hist(random_number_1);
 axs.hist();
 
 plt.legend();
