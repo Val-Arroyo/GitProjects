bc9db136db08af7ad9de5bc6f46c20c4dce14745
valarroyo-gif <valarroyo39@gmail.com>
2023-09-27 20:18:55 +1300

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -9,4 +9,4 @@ from numpy.lib.histograms import histogram
 random_number_1 = np.random.normal(240, 10, 240);
 random_numner_2 = np.random.normal(140, 10, 240);
 
-fig, axs = plt.subplots(1, 1)
+fig, axs = plt.subplots(1, 1, figsize = )
