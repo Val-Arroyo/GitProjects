3a2dab78313e6ed9d23aeb7c3351fb4e962f46f8
valarroyo-gif <valarroyo39@gmail.com>
2023-09-27 20:17:26 +1300

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -7,4 +7,4 @@ from matplotlib.ticker import PercentFormatter
 from numpy.lib.histograms import histogram
 
 random_number_1 = np.random.normal(240, 10, 250);
-random_numner_2 = np.random.normal(140, 10,);
+random_numner_2 = np.random.normal(140, 10, 240);
