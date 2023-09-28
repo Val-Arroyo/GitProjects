5e9969c84ae129225ec63b18e98d6c9ef7559c79
valarroyo-gif <valarroyo39@gmail.com>
2023-09-28 15:16:55 +1300

    Update Scatterplot.py

--- GraphProjects/Studysession/Scatterplot.py
+++ GraphProjects/Studysession/Scatterplot.py
@@ -12,6 +12,6 @@ y_value = np.array([10, 30, 50, 70, 90, 110]);
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
-axs.scatter(x_value, y_value, color = 'red', marker = '^', edgecolors = 'black');
+axs.scatter(x_value, y_value, color = 'red', marker = '^', edgecolors = 'black', size = 0.5);
 
 plt.show();
