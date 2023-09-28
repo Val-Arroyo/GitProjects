eee7ec8990652eef23ef1b9eaadb530128d9c451
valarroyo-gif <valarroyo39@gmail.com>
2023-09-28 15:37:31 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -8,12 +8,12 @@ from matplotlib.ticker import PercentFormatter
 #Dataset
 name = ["Mercedes", "AUDI", "BMW", "TESLA", "FORD"];
 
-data = [45, 15, 10, 20, 5, 5];
+data = [45, 15, 10, 20, 10];
 
-explode = [0.5, 0.5, 0.5, 0.5, 0.5];
+Explode = [0.5, 0.5, 0.5, 0.5, 0.5];
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5));
 
-axs.pie(data, name);
+axs.pie(data, labels = name, explode = Explode);
 
 plt.show();
