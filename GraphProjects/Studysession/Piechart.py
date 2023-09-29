86bf58b013f6b5e6c1a24a13cbe60f98c9d592c1
valarroyo-gif <valarroyo39@gmail.com>
2023-09-29 21:24:02 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -9,3 +9,5 @@ phone_brands = ["Apple", "Samsung", "Oppo", "Oneplus", "Xiaomi"];
 phone_data = [45, 15, 10, 20, 10];
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
+
+axs.pie();
