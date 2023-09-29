e63ed3e35543f076cb50bcf3596688cab81a15d7
valarroyo-gif <valarroyo39@gmail.com>
2023-09-29 21:20:58 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -5,5 +5,5 @@ import pandas as pd
 from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
-phone_brands = ["Apple", "Samsung", "Oppo", "Oneplus", "Xiamu"];
+phone_brands = ["Apple", "Samsung", "Oppo", "Oneplus", "Xiaomi"];
 phone_data = [];
