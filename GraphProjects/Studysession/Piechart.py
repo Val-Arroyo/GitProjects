b4ba560d2b5b6f633796e953187527c8cb9f016c
valarroyo-gif <valarroyo39@gmail.com>
2023-09-29 21:24:38 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -10,6 +10,6 @@ phone_data = [45, 15, 10, 20, 10];
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
-colors = [];
+colors = ["Grey", "Blue"];
 
 axs.pie(phone_brands, phone_data, );
