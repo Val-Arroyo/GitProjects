9b75e8d82c1b2c1177a1a6d3143602ebd1537002
valarroyo-gif <valarroyo39@gmail.com>
2023-09-29 21:23:29 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -8,4 +8,4 @@ from matplotlib.ticker import PercentFormatter
 phone_brands = ["Apple", "Samsung", "Oppo", "Oneplus", "Xiaomi"];
 phone_data = [45, 15, 10, 20, 10];
 
-fig, axs = plt.subplots(1, 1, );
+fig, axs = plt.subplots(1, 1, figsize = (7, 5));
