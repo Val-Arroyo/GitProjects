41a3c6d72e5b68422c0ef668822c5c0daa758b95
valarroyo-gif <valarroyo39@gmail.com>
2023-10-03 15:27:36 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -11,7 +11,7 @@ sales = [45.0, 25.0, 15.0, 15.0];
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
 Space = (0.1, 0.1, 0.1, 0.1);
-colors = ("");
+colors = ("grey",);
 
 axs.pie(sales, labels = brands, shadow = True, explode = Space)
 
