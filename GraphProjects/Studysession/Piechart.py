2ad5a192d0ff4ac020463774f169a221cbf03acd
valarroyo-gif <valarroyo39@gmail.com>
2023-10-03 15:20:21 +1300

    Update Piechart.py

--- GraphProjects/Studysession/Piechart.py
+++ GraphProjects/Studysession/Piechart.py
@@ -12,6 +12,6 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
 Space = (0.1, 0.1, 0.1, 0.1);
 
-axs.pie(sales, label = brands, shadow=)
+axs.pie(sales, label = brands, shadow = )
 
 plt.show();
