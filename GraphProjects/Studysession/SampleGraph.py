72fa9cada55e1dca9dc9e15cae50255ea2942f2b
valarroyo-gif <valarroyo39@gmail.com>
2023-10-05 20:08:44 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -14,7 +14,7 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 colors = ("grey", "blue");
 
 axs.pie(percentage, labels = brands, colors = colors, shadow = True,
-        autopct= '%1.1f%%', startangle=);
+        autopct= '%1.1f%%', startangle = 0);
 
 plt.grid();
 
