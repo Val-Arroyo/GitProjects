126e26a6af848002f28bbb4de16a332233b64e6d
valarroyo-gif <valarroyo39@gmail.com>
2023-10-09 20:22:15 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -1,11 +1,5 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from matplotlib import colors
-from matplotlib.ticker import PercentFormatter
-
-brands = ["Apple", "Samsung"];
-percentage = [65, 35];
-
-fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
+from matplotlib import colors 
