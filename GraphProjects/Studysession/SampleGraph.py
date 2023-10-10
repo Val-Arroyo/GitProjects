fec115a596dd01d8114219ff26ef29404cd3b254
valarroyo-gif <valarroyo39@gmail.com>
2023-10-10 18:46:14 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -13,7 +13,7 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 colors = ("grey", "blue");
 explode = (0.1, 0.1)
 axs.pie(percentage, labels = brands, shadow = True, autopct = "%1.1f%%",
-        colors = colors,
+        colors = colors, explode = explode,
         wedgeprops = {"edgecolor": "black",
                       "linewidth": 3,
                       "antialiased": True} );
