a97cb9fb66a7baf6889cf2c0214c46b9b897e09c
valarroyo-gif <valarroyo39@gmail.com>
2023-10-10 18:42:16 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -10,7 +10,7 @@ percentage = [65.0, 35.0];
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
-colors = ("grey", "blue")
+colors = ("grey", "blue");
 axs.pie(percentage, labels = brands, shadow = True, 
         wedgeprops= "");
 
