77ed9c4b461984646abdab5b994bda3252a1f4a7
valarroyo-gif <valarroyo39@gmail.com>
2023-10-10 18:35:26 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -8,4 +8,4 @@ from matplotlib import colors
 brands = ["Apple", "Samsung"];
 percentage = [65.0, 35.0];
 
-fig, axs = plt.subplots(1, 1, figsize = );
+fig, axs = plt.subplots(1, 1, figsize = (7, 5));
