e6198660219dc19bdc9ff2f92001c206943c5510
valarroyo-gif <valarroyo39@gmail.com>
2023-10-11 20:05:40 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -13,4 +13,10 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 colors = ("grey", "blue");
 explode = (0.1, 0.1);
 
-axs.pie(percent, labels = brands, colors = colors, shadow = True)
+axs.pie(percent, labels = brands, colors = colors, shadow = True,
+        autopct= "%1.1f%%", explode = explode,
+        wedgeprops = {"linewidth": 3,
+                      "edgecolor": "black",
+                      "antialiased": True})
+
+plt.show();
