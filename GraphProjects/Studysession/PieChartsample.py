af71e26d5d02b437f9132341f64684ea2863a619
valarroyo-gif <valarroyo39@gmail.com>
2023-10-11 20:02:34 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -10,4 +10,5 @@ percent = [65, 35];
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
-axs.pie(percent, labels = brands)
+colors = ("grey", "blue")
+axs.pie(percent, labels = brands, )
