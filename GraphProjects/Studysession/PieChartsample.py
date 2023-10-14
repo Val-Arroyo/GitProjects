fbef13783ff2893491b0503d1abb7fe12e5ca304
valarroyo-gif <valarroyo39@gmail.com>
2023-10-14 21:31:05 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from matplotlib.ticker
+from matplotlib.ticker import 
 
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [55.0, 25.0, 5.0, 15.0 ];
