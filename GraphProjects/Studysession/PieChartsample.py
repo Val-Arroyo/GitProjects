dc1d0bf379172f6ae9a2776ce05d844b49ef4c56
valarroyo-gif <valarroyo39@gmail.com>
2023-10-15 20:14:28 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -6,7 +6,7 @@ import pandas as pd
 from matplotlib.ticker import PercentFormatter
 from matplotlib import colors 
 
-brands = [];
+brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0]
 
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
