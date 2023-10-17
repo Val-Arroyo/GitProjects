9df4a6de86e80a1e57557e6bf65dc6cf7b93f60c
valarroyo-gif <valarroyo39@gmail.com>
2023-10-17 20:27:08 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -18,5 +18,5 @@ axs.pie(percent, labels = brands, explode = explode, colors = colors,
                      "linewidth": 3,
                      "antialiased": True});
 
-
+plt.axis()
 plt.show();
