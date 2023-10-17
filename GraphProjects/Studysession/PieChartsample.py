30a58812a9a0936998e6c5ccb408a88ad732897a
valarroyo-gif <valarroyo39@gmail.com>
2023-10-17 20:21:28 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -8,4 +8,4 @@ from matplotlib import colors
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
 
-fig, axs = plt.subplots(1, 1, figsize = (7, 5));
+fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
