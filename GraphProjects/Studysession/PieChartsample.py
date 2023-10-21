6595f97c305833d58b3e37b20e0d015f8a2f7832
valarroyo-gif <valarroyo39@gmail.com>
2023-10-21 21:58:14 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -12,5 +12,5 @@ percent = []
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True);
 
 explode = (0.1, 0.1, 0.1, 0.1);
-colors = ("grey", "blue", "white", "cyan")
+colors = ("grey", "blue", "white", "cyan");
 axs.pie();
