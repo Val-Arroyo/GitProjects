4fcbd866b5f8117039820199a626078a427c4d2f
valarroyo-gif <valarroyo39@gmail.com>
2023-10-23 20:16:44 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -9,4 +9,4 @@ from matplotlib import colors
 brands = [];
 percent = [];
 
-fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout )
+fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout  = True)
