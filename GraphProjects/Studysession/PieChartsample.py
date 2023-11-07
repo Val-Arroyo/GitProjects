1e1692f1cc4728390e1027616758d1435c137d57
valarroyo-gif <valarroyo39@gmail.com>
2023-11-07 18:23:24 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -17,4 +17,5 @@ explode = (0.1, 0.1, 0.1, 0.1)
 axs.pie(percent, labels = brands, colors = colors,
         explode = explode, autopct= "%1.1f%%", shadow = True,
         wedgeprops = {"antialiased": True,
-                      "linewidth": 3});
+                      "linewidth": 3,
+                      });
