5a5205447b3079e25e4d31110a7ef36a06c1aafc
valarroyo-gif <valarroyo39@gmail.com>
2023-11-08 20:16:25 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -15,4 +15,4 @@ colors = ("grey", "blue", "white", "cyan")
 explode = (0.1, 0.1, 0.1, 0.1);
 
 axs.pie(percent, labels = brands, autopct = "%1.1f%%",
-        colors = )
+        colors = colors, )
