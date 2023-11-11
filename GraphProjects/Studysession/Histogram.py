ea259f08dcd52641d3f897d9ea42299139e2c59e
valarroyo-gif <valarroyo39@gmail.com>
2023-11-11 17:48:45 +1300

    Update Histogram.py

--- GraphProjects/Studysession/Histogram.py
+++ GraphProjects/Studysession/Histogram.py
@@ -14,7 +14,7 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
 axs.hist(random_number_1, color = 'black');
 axs.hist(random_numner_2, color = 'black');
 
-
+plt.stlye.use();
 
 plt.legend();
 plt.grid();
