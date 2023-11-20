63fe7ac3661f06b411395f19bb15e35eacf93ba6
valarroyo-gif <valarroyo39@gmail.com>
2023-11-20 18:42:32 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -3,6 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib.ticker import PercentFormatter
+
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
 
@@ -12,6 +13,7 @@ colors = ("grey", "blue", "red", "cyan");
 explode = (0.1, 0.1, 0.1, 0.1); seaborn as sns
 import pandas as pd
 from matplotlib.ticker import PercentFormatter
+
 axs.pie(percent, labels = brands, colors = colors, 
         autopct = "%1.1f%%", explode = explode, shadow = True,
         wedgeprops = {"antialiased": True,
