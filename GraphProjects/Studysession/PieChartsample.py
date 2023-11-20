1e891698953e213b002fa6c7580b399b0a5ebd65
valarroyo-gif <valarroyo39@gmail.com>
2023-11-20 18:42:03 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import pandas as 
+import pandas as pd
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
 
@@ -9,7 +9,7 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
 
 colors = ("grey", "blue", "red", "cyan");
 explode = (0.1, 0.1, 0.1, 0.1); seaborn as sns
-import pandas as 
+import pandas as pd
 axs.pie(percent, labels = brands, colors = colors, 
         autopct = "%1.1f%%", explode = explode, shadow = True,
         wedgeprops = {"antialiased": True,
@@ -20,4 +20,4 @@ plt.grid();
 plt.axis("equal");
 plt.legend();
 plt.show() seaborn as sns
-import pandas as 
+import pandas as pd
