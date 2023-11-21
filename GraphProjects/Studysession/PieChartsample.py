86a056fda7998f8e42fbad46ea2310aed655ae10
valarroyo-gif <valarroyo39@gmail.com>
2023-11-21 20:07:10 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -1,12 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import pandas as pd
-from matplotlib.ticker import PercentFormatter
-from matplotlib import style
-from matplotlib import colors 
 
-plt.style.use("dark_background")
 
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
@@ -14,7 +6,7 @@ percent = [45.0, 25.0, 10.0, 20.0];
 fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
 
 colors = ("grey", "blue", "red", "cyan");
-explode = (0.1, 0.1, 0.1, 0.1); 
+explode = (0.1, 0.1, 0.1, 0.1);         
 
 axs.pie(percent, labels = brands, colors = colors, 
         autopct = "%1.1f%%", explode = explode, shadow = True,
