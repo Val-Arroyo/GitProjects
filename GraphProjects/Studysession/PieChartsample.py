29d8f8727ea751e8cb8a2dc3ba25baeb05cfef3b
valarroyo-gif <valarroyo39@gmail.com>
2023-12-27 18:08:02 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -5,6 +5,7 @@ import pandas as pd
 from matplotlib.ticker import PercentFormatter
 from matplotlib import style
 
+
 plt.style.use("dark_background");
 
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
