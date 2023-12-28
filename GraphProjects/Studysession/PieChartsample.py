71c725cbc407789a664d2dac432f330603236aee
valarroyo-gif <valarroyo39@gmail.com>
2023-12-28 13:52:42 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -11,7 +11,7 @@ plt.style.use("dark_background");
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
 
-fig
+fig, axs 
 
 colors = ("grey", "blue", "red", "cyan");
 explode = (0.1, 0.1, 0.1, 0.1);         
