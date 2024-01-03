cda8a691c3af425fc53d710c5f648b17daf73308
valarroyo-gif <valarroyo39@gmail.com>
2024-01-04 11:19:09 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -17,7 +17,7 @@ colors = ("grey", "blue", "red", "cyan");
 explode = (0.1, 0.1, 0.1, 0.1);         
 
 axs.pie(percent, labels= brands, colors= colors, 
-        explode= explode, startangle= 0, shadow= True, autopct=,
+        explode= explode, startangle= 0, shadow= True, autopct= "%1f",
         wedgeprops= {"edgecolor": "black",
                      "linewidth": 3,
                      "antialiased": True})
