594e0c7b7e067453b22b3fd59396c1c53987ffcf
valarroyo-gif <valarroyo39@gmail.com>
2024-01-03 17:06:15 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -19,7 +19,7 @@ explode = (0.1, 0.1, 0.1, 0.1);
 axs.pie(percent, labels = brands, explode = explode,
         autopct= "%1.1f%%", startangle = 0,
         wedgeprops = {"antialiased": True,
-                      "linewidth"})
+                      "linewidth":})
          
 plt.grid()
 plt.legend()
