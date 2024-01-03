3a57227e61ded5fa04112de47335faffaa7ee269
valarroyo-gif <valarroyo39@gmail.com>
2024-01-04 11:17:57 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -18,7 +18,7 @@ explode = (0.1, 0.1, 0.1, 0.1);
 
 axs.pie(percent, labels= brands, colors= colors, 
         explode= explode, startangle= 0, shadow= True,
-        wedgeprops= {"edgecolor"})
+        wedgeprops= {"edgecolor":})
          
 plt.grid()
 plt.legend()
