7601999c46506091a3de62f40e676e5bbce2bdf0
valarroyo-gif <valarroyo39@gmail.com>
2024-02-28 15:51:36 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -1,10 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np      
-import pandas as pd
-import seaborn as sns
-from matplotlib.ticker import PercentFormatter
-from matplotlib import colors
-from matplotlib import style
+
 
 plt.style.use("dark_background");
 
