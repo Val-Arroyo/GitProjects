f368f6f4cb9d6382461fcb9cb20a5c4f1ea21f48
valarroyo-gif <valarroyo39@gmail.com>
2024-02-29 18:11:23 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -16,7 +16,7 @@ fig, axs = plt.subplots(1, 1, figsize = (7, 5), tight_layout = True)
 colors = ("grey", "blue", "red", "cyan");
 explode = (0.1, 0.1, 0.1, 0.1);         
 
-axs.pie(percent, labels = brands, )
+axs.pie(percent, labels = brands, colors )
       
 plt.grid()
 plt.legend()
