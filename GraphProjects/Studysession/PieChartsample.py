37d13d5fea3b62005c40a62e3bc91414f59d0a06
valarroyo-gif <valarroyo39@gmail.com>
2024-03-04 15:22:26 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as 
+import seaborn as sns
+
 
 brands = ["Apple", "Samsung", "Google", "OnePlus"];
 percent = [45.0, 25.0, 10.0, 20.0];
