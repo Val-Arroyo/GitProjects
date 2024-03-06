d6875aebb2c0d3cdaa95edd810e5d10b9392de70
valarroyo-gif <valarroyo39@gmail.com>
2024-03-06 15:50:36 +1300

    Update SampleGraph.py

--- GraphProjects/Studysession/SampleGraph.py
+++ GraphProjects/Studysession/SampleGraph.py
@@ -17,6 +17,6 @@ axs.pie(percentage, labels = brands, shadow = True, autopct = "%1.1f%%",
         wedgeprops = {"edgecolor": "black",
                       "linewidth": 3,
                       "antialiased": True} );
-axs.set_title
+axs.set_title()
 
 plt.show();
