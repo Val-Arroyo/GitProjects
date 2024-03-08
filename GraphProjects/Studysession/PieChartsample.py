420cf2a42732a2c576ace94395d6ebc3f9cecf63
valarroyo-gif <valarroyo39@gmail.com>
2024-03-08 21:09:35 +1300

    Update PieChartsample.py

--- GraphProjects/Studysession/PieChartsample.py
+++ GraphProjects/Studysession/PieChartsample.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from matplotlib.ticker 
+from matplotlib.ticker as 
 
 plt.style.use("dark_background")
 
