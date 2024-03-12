32e6c458859248430dca9da53f54e0dad9a4ba7a
valarroyo-gif <valarroyo39@gmail.com>
2024-03-12 15:42:41 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -6,8 +6,10 @@ z = np.random.randint(100, size = (50));
 x = np.random.randint(80, size = (50));
 y = np.random.randint(60, size = (50));
 
-fig = plt.figure();
+fig = plt.figure(figsize = (10, 7));
 
 ax = plt.axes(projection = "3d");
+
+
 
 plt.show();
