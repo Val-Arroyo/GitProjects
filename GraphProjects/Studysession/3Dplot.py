a4c57750d20ad5c3c78b95a0789664fa95b0bbe9
valarroyo-gif <valarroyo39@gmail.com>
2024-03-12 15:45:34 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -6,10 +6,12 @@ z = np.random.randint(100, size = (50));
 x = np.random.randint(80, size = (50));
 y = np.random.randint(60, size = (50));
 
-fig = plt.figure(figsize = (10, 7));
+plt.style.use("")
 
+fig = plt.figure(figsize = (10, 7));
 ax = plt.axes(projection = "3d");
 
-
+ax.scatter3D(x, y, z, color = "white")
+plt.title("Simple 3D Scatter Plot")
 
 plt.show();
