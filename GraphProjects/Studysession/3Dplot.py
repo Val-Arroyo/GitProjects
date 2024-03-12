6ce5494c57763ff3857a5dedd6a17232d79e3686
valarroyo-gif <valarroyo39@gmail.com>
2024-03-12 15:45:38 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -6,7 +6,7 @@ z = np.random.randint(100, size = (50));
 x = np.random.randint(80, size = (50));
 y = np.random.randint(60, size = (50));
 
-plt.style.use("")
+plt.style.use("dark_background")
 
 fig = plt.figure(figsize = (10, 7));
 ax = plt.axes(projection = "3d");
