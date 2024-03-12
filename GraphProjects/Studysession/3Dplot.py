5434d22a2a8a4042f9ee569dd64501afc533df8e
valarroyo-gif <valarroyo39@gmail.com>
2024-03-12 15:41:41 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d
 
-z = np.random.randint(100, size =(50));
+z = np.random.randint(100, size = (50));
 x = np.random.randint(80, size = (50));
 y = np.random.randint(60, size = (50));
 
