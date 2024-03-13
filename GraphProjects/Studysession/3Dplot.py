7dbdd8bb88e67f728f03a76740536776fc1e2f52
valarroyo-gif <valarroyo39@gmail.com>
2024-03-13 16:05:50 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -2,16 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits import mplot3d
 
-z = np.random.randint(100, size = (50));
-x = np.random.randint(80, size = (50));
-y = np.random.randint(60, size = (50));
+
 
 plt.style.use("dark_background")
 
 fig = plt.figure(figsize = (10, 7));
 ax = plt.axes(projection = "3d");
 
-ax.scatter3D(x, y, z, color = "white")
+ax.hist3D(x, y, z, color = "white")
 plt.title("Simple 3D Scatter Plot")
 
 plt.show();
