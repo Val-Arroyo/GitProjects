e6ee2122ade42cb0aba0e36bf32149ec1fec72d3
valarroyo-gif <valarroyo39@gmail.com>
2024-03-13 16:34:57 +1300

    Update 3Dplot.py

---
+++ GraphProjects/Studysession/3Dplot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 3, 4, 5])
+z = np.zeros_like(x)
+
+dx = np.ones_like(x)
+dy = np.ones_like(y)
+dz = [1, 2, 3, 4, 5]  # Heights of the bars
+
+# Create 3D bar graph
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.bar3d(x, y, z, dx, dy, dz, color='skyblue')
+
+ax.set_xlabel('X Axis')
+ax.set_ylabel('Y Axis')
+ax.set_zlabel('Z Axis')
+
+plt.title('3D Bar Graph Example')
+plt.show()
