25a1e0f12d10a81256c3e934fcc14f2bd48bc570
valarroyo-gif <valarroyo39@gmail.com>
2024-03-15 21:13:23 +1300

    Update 3Dplot.py

---
+++ GraphProjects/Studysession/3Dplot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Data for the stacked bar graph
+x = np.array([1, 2, 3, 4, 5])  # X coordinates
+y = np.array([1, 2, 3, 4, 5])  # Y coordinates
+z = np.zeros(len(x))  # Z coordinates (start at 0 for the first layer)
+dx = np.ones(len(x))  # Width of each bar along the x-axis
+dy = np.ones(len(y))  # Width of each bar along the y-axis
+dz1 = np.array([1, 2, 3, 4, 5])  # Heights of the first stack
+dz2 = np.array([2, 3, 4, 5, 6])  # Heights of the second stack
+
+# Create a 3D figure
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the first stack
+ax.bar3d(x, y, z, dx, dy, dz1, color='b')
+
+# Plot the second stack on top of the first stack
+ax.bar3d(x, y, dz1, dx, dy, dz2, color='r')
+
+# Customize labels and title
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+plt.title('Stacked Bar Graph in 3D')
+
+# Show the plot
+plt.show()
