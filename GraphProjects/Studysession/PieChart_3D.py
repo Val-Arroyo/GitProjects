873ed214ed7f085675039772858f64e282a913e8
valarroyo-gif <valarroyo39@gmail.com>
2024-03-15 21:16:27 +1300

    Update PieChart_3D.py

--- GraphProjects/Studysession/PieChart_3D.py
+++ GraphProjects/Studysession/PieChart_3D.py
@@ -1,30 +1,28 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Data for the pie chart
-labels = ['A', 'B', 'C', 'D', 'E']
-sizes = [15, 30, 25, 10, 20]
+# Generate data
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
 
-# Create a 3D-like effect by plotting labels
-fig = plt.figure(figsize=(8, 8))
+# Create 3D plot
+fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Pie chart parameters
-explode = (0, 0, 0, 0, 0.1)  # explode the last slice
-colors = plt.cm.tab10(np.arange(len(labels)))  # color palette
+# Plot 3D surface
+surf = ax.plot_surface(x, y, z, cmap='viridis')
 
-# Plot pie chart slices and labels
-ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=140, colors=colors)
+# Customize labels and title
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('3D Surface Plot')
 
-# Draw a circle at the center of the pie to make it look like a donut chart
-center_circle = plt.Circle((0, 0), 0.70, fc='white')
-ax.add_artist(center_circle)
-
-# Equal aspect ratio ensures that pie is drawn as a circle
-ax.axis('equal')
-
-# Set title
-plt.title('3D-like Pie Chart')
+# Add a color bar which maps values to colors
+fig.colorbar(surf)
 
 # Show plot
 plt.show()
