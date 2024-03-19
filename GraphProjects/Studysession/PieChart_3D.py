fa882387f4c91d2ad7e8e28ed04cf13cbeb207df
valarroyo-gif <valarroyo39@gmail.com>
2024-03-19 18:36:27 +1300

    Update PieChart_3D.py

--- GraphProjects/Studysession/PieChart_3D.py
+++ GraphProjects/Studysession/PieChart_3D.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import pandas as pd
-from mpl_toolkits.mplot3d import Axes3D
+
 
 # Generate data for the surface plot
 x_surf = np.linspace(-5, 5, 100)
