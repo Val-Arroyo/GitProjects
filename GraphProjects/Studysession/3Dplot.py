0b40ac508b17ce482352c2857d1aaab6215368f2
valarroyo-gif <valarroyo39@gmail.com>
2024-03-23 14:19:26 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib import colors
-from matplotlib import 
+from matplotlib import style
 from mpl_toolkits.mplot3d import Axes3D
 
 # Data for the stacked bar graph
