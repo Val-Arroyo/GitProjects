623a81d49a782bdb6e789debf4198a77145fd9e5
valarroyo-gif <valarroyo39@gmail.com>
2024-03-23 13:39:18 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import pandas as pd
-from mpl_toolkits.mplot3d import Axes3D
+
 
 # Data for the stacked bar graph
 x = np.array([1, 2, 3, 4, 5])  # X coordinates
