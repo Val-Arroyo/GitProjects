65e31eed75da5a2bad16fad1532aa273fa7ad1c3
valarroyo-gif <valarroyo39@gmail.com>
2024-03-25 18:12:30 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from matplotlib import colors
-from matplotlib
+from matplotlib import 
 
 # Data for the stacked bar graph
 x = np.array([1, 2, 3, 4, 5])  # X coordinates
