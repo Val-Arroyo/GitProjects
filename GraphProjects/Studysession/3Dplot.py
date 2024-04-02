d8aeff2eba674bafc635cc6c4ee203addb2b1ef1
valarroyo-gif <valarroyo39@gmail.com>
2024-04-02 20:05:17 +1300

    Update 3Dplot.py

--- GraphProjects/Studysession/3Dplot.py
+++ GraphProjects/Studysession/3Dplot.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import pandas as pd
 from matplotlib import colors 
 from matplotlib import style
-from mpl_toolkits.mplot3d import 
+from mpl_toolkits.mplot3d import Axes3D
 
 plt.style.use("dark_background");
 
