2be8732856b6fe05ff6580d5c4320b413f742484
valarroyo-gif <valarroyo39@gmail.com>
2024-04-03 16:35:52 +1300

    Update 3Dgraph.py

---
+++ GraphProjects/Studysession/3Dgraph.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Triangulation
+
+# Generate some random data
+x = np.random.rand(100)
+y = np.random.rand(100)
+z = np.sin(x * y * np.pi)  # Some arbitrary function
+
+# Create a triangulation from the data
+tri = Triangulation(x, y)
+
+# Create a figure and a 3D axis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the triangular surface
+ax.plot_trisurf(tri, z, cmap='viridis')
+
+# Set labels and title
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Triangular Surface Plot')
+
+# Show the plot
+plt.show()
