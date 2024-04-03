aa1107738ee13f277a3213f4a880c88b8781184a
valarroyo-gif <valarroyo39@gmail.com>
2024-04-03 16:36:33 +1300

    Update 3Dgraph.py

--- GraphProjects/Studysession/3Dgraph.py
+++ GraphProjects/Studysession/3Dgraph.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits.mplot3d import Triangulation
 
 # Generate some random data
 x = np.random.rand(100)
