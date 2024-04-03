e2bf69fd835489b6d48eb2b8c55fe7d5bba08e2b
valarroyo-gif <valarroyo39@gmail.com>
2024-04-03 15:42:51 +1300

    Update 3Dgraph.py

--- GraphProjects/Studysession/3Dgraph.py
+++ GraphProjects/Studysession/3Dgraph.py
@@ -1,12 +1,18 @@
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Load sample dataset
-tips = sns.load_dataset("tips")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# Plot a simple scatter plot
-sns.scatterplot(x="total_bill", y="tip", data=tips)
+u = np.linspace(0, 2*np.pi, 100)
+v = np.linspace(-1, 1, 100)
+U, V = np.meshgrid(u, v)
+X = (1 + 0.5*V*np.cos(U/2))*np.cos(U)
+Y = (1 + 0.5*V*np.cos(U/2))*np.sin(U)
+Z = 0.5*V*np.sin(U/2)
 
-plt.title('Simple Scatter Plot')
+ax.plot_surface(X, Y, Z, rstride=5, cstride=5, color='c', edgecolors='k')
 
 plt.show()
+
