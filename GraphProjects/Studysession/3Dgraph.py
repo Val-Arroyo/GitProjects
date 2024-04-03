6fb49012490faad83c7636e898aa63f1ea8cfb12
valarroyo-gif <valarroyo39@gmail.com>
2024-04-03 16:41:28 +1300

    Update 3Dgraph.py

--- GraphProjects/Studysession/3Dgraph.py
+++ GraphProjects/Studysession/3Dgraph.py
@@ -1,1 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+# Generate data
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
+
+# Create a figure and a 3D axis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the 3D surface
+surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Add a color bar which maps values to colors
+fig.colorbar(surf)
+
+# Set labels and title
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('3D Surface Plot')
+
+# Show the plot
+plt.show()
