774bd3444c03e650c37b67977487c47bc3b2e05d
valarroyo-gif <valarroyo39@gmail.com>
2024-04-06 12:11:12 +1300

    Update 3Dgraph.py

--- GraphProjects/Studysession/3Dgraph.py
+++ GraphProjects/Studysession/3Dgraph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import pandas as pd
-from matplotlib import 
+from matplotlib import style
 
 # Generate data
 x = np.linspace(-5, 5, 100)
