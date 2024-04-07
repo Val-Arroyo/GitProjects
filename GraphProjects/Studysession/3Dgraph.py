f4f44d89f2d7cd4a9efe8c18e049c867015e2111
valarroyo-gif <valarroyo39@gmail.com>
2024-04-07 19:59:51 +1200

    Update 3Dgraph.py

--- GraphProjects/Studysession/3Dgraph.py
+++ GraphProjects/Studysession/3Dgraph.py
@@ -6,7 +6,7 @@ from matplotlib import colors
 from matplotlib import style
 from mpl_toolkits.mplot3d import Axes3D
 
-plt.style.use("dark_")
+plt.style.use("dark_background")
 
 # Generate data
 x = np.linspace(-5, 5, 100)
